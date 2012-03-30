6101ef090593aafe4c06a22df249a5f201a519f4
Steven Githens <swgithen@mtu.edu>
2012-03-30 16:54:23 -0400

    Adding keyring support

--- htmlform.py
+++ htmlform.py
@@ -1,18 +1,49 @@
+import ConfigParser
+import getpass
 import httplib2
+import keyring
+import os
 import sys
 import urllib
 
-#serverprefix="http://localhost:8081/openmrs-standalone"
-serveruname=""
-serverpasswd=""
-
 disable_ssl_certificate_validation = True
+
+def get_settings():
+    config_file = os.path.expanduser('~/.openmrs-logins.cfg')
+    config = ConfigParser.SafeConfigParser({
+            'username':'',
+            'serverprefix':'',
+            })
+    config.read(config_file)
+    if not config.has_section('openmrs_form_server'):
+        config.add_section('openmrs_form_server')
+
+    username = config.get('openmrs_form_server','username')
+    serverprefix = config.get('openmrs_form_server','serverprefix')
+    password = None
+
+    if username != '' and serverprefix != '':
+        password = keyring.get_password('openmrs_form_server',username)
+        
+    if password == None: # or not auth(username,password) <- random auth test function
+        serverprefix = raw_input("Forms Server Prefix:\n")
+        username = raw_input("Username:\n")
+        password = getpass.getpass("Password:\n")
+        config.set('openmrs_form_server','username',username)
+        config.set('openmrs_form_server','serverprefix',serverprefix)
+        config.write(open(config_file,'w'))
+        keyring.set_password('openmrs_form_server',username,password)
+
+    return [username,password,serverprefix]
+            
+
 def main(args):
+    uname,passwd,serverprefix = get_settings()
     formid = args[0]
     file = args[1]
     f = open(file)
     h = httplib2.Http(disable_ssl_certificate_validation=disable_ssl_certificate_validation)
-    data = {'uname':serveruname,'pw':serverpasswd}
+    data = {'uname':uname,'pw':passwd}
     body = urllib.urlencode(data)
     resp, content = h.request(serverprefix+"/loginServlet", method="POST", body=body, headers={'Content-type': 'application/x-www-form-urlencoded'})
     print resp
